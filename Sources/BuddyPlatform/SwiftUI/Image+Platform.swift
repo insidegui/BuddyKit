dc61a0718b28ad167b0dda0131d058feabe52825
Guilherme Rambo <insidegui@gmail.com>
2024-10-31 10:59:09 -0300

    Add SwiftUI Image extension for PlatformImage

---
+++ Sources/BuddyPlatform/SwiftUI/Image+Platform.swift
@@ -0,0 +1,13 @@
+#if !os(watchOS)
+import SwiftUI
+
+public extension Image {
+    init(platformImage: PlatformImage) {
+        #if os(macOS)
+        self.init(nsImage: platformImage)
+        #else
+        self.init(uiImage: platformImage)
+        #endif
+    }
+}
+#endif // !os(watchOS)
