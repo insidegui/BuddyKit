715a3976dcf55094ee26daf28358c33c353ef180
Guilherme Rambo <insidegui@gmail.com>
2024-11-08 10:22:38 -0300

    Add docs to Bool+Require

--- Sources/BuddyFoundation/Foundation/Bool+Require.swift
+++ Sources/BuddyFoundation/Foundation/Bool+Require.swift
@@ -1,4 +1,7 @@
 public extension Bool {
+    /// Throws an error if the bool value is `false`.
+    /// - Parameter error: The error to throw if the value is `false`.
+    /// - Returns: `true` if the value was true (discardable result).
     @discardableResult
     func require(_ error: Error) throws -> Self {
         guard self else { throw error }
